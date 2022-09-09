3857f2da56671129152df80b85302b24cc05f77f
Stanislav <stanislav.moiseevskiy@gmail.com>
2022-09-09 15:25:56 +0300

    create app

---
+++ AngryFileSorter/AngryFileSorter/PrintHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace AngryFileSorter;
+
+public static class PrintHelper
+{
+    public static void Print(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ForegroundColor = ConsoleColor.White;
+
+        
+    }
+
+    public void SendResponse()
+    {
+        var inviteToPivnuha = GetRequest(requestor: Mikhail);
+        ProccessRequest(inviteToPivnuha);
+    }
+
+    public bool ProccessRequest(PivnuhaRequest inviteToPivnuha)
+    {
+        if (inviteToPivnuha.Status == ReceivedFromMikhail)
+        {
+            var acceptedBySelfMatters = await CheckSelfMatters(AcceptIfEmpty);
+            var dayType = CheckDay(DateTime.Now);
+
+            if (acceptedBySelfMatters && dayType is { holiday, weekend, vacation }) // true
+            {
+                SendAcceptedAnswer(receiver: Mikhail);
+                SendMeetingTime(receiver: realPacany, TimeOnly: 21:00);
+            }
+            else
+            {
+                throw new BadAnswerException(MessageProcessingHandler: "lets try other day");
+            }
+        }
+    }
+}
---
+++ AngryFileSorter/AngryFileSorter/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Configuration;
+using AngryFileSorter;
+
+PrintHelper.Print("Запуск...", ConsoleColor.Yellow);
+
+var fromDirectory = string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings.Get("AutoScanFolderPath"))
+    ? throw new ArgumentException("Ошибка папки сбора изображений")
+    : ConfigurationManager.AppSettings.Get("AutoScanFolderPath");
+
+var files = Directory.GetFiles(fromDirectory!, "*.*", SearchOption.AllDirectories);
+
+var processingImageFormats = ConfigurationManager.AppSettings.Get("ProcessingFileFormats")
+    ?.Split(",")
+    .Where(x => !string.IsNullOrWhiteSpace(x))
+    .Select(x => x.Replace(" ", ""))
+    .ToArray();
+
+if (processingImageFormats != null && processingImageFormats.Length != 0)
+{
+    files = files
+        .Where(x => processingImageFormats.Contains(Path.GetExtension(x).ToLower()))
+        .ToArray();
+}
+else
+{
+    PrintHelper.Print("Не указаны форматы изображений для обработки. Будут обработаны файлы всех форматов.",
+        ConsoleColor.Yellow);
+}
+
+PrintHelper.Print($"Надено файлов для обработки = {files.Length}", ConsoleColor.Yellow);
+
+var path = ConfigurationManager.AppSettings.Get("ResultFolderPath");
+
+if (string.IsNullOrWhiteSpace(path))
+{
+    path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+}
+
+var newFolderName = Guid.NewGuid().ToString().Replace("-", "");
+var newFolderFullPath = Path.Combine(path, newFolderName);
+
+Directory.CreateDirectory(newFolderFullPath);
+
+var needToDeleteSourceFile = Convert.ToBoolean(ConfigurationManager.AppSettings.Get("NeedToDeleteSourceFile"));
+MoveFiles(files, newFolderFullPath, needToDeleteSourceFile);
+
+void MoveFiles(string[] selectedFiles, string resultFolderFullPath, bool needToDeleteSource)
+{
+    var rangeToProcess = Convert.ToInt32(ConfigurationManager.AppSettings.Get("FolderItemsMaxLength"));
+    var iterator = 1;
+
+    while (selectedFiles.Length > 0)
+    {
+        var processingFiles = selectedFiles.Take(rangeToProcess).ToArray();
+
+        foreach (var processingFile in processingFiles)
+        {
+            var newPath = Path.Combine(resultFolderFullPath, iterator.ToString());
+            Directory.CreateDirectory(newPath);
+            var fileName = Path.GetFileName(processingFile);
+            var resultFile = Path.Combine(newPath, fileName);
+
+            if (needToDeleteSource)
+            {
+                File.Move(processingFile, resultFile);
+            }
+            else
+            {
+                File.Copy(processingFile, resultFile);
+            }
+
+            PrintHelper.Print($"Перемещен файл {processingFile} >>> {resultFile}", ConsoleColor.DarkCyan);
+        }
+
+        selectedFiles = selectedFiles.Except(processingFiles).ToArray();
+        iterator++;
+    }
+}
